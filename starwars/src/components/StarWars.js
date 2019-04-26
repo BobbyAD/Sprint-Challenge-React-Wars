c6233d2e2c26d9c5568aad3d15da49a43770b3a3
Robert Driskell <driskell.ba@gmail.com>
2019-04-26 11:58:40 -0500

    Got Homeworld and Species rendering

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,21 +1,69 @@
 import React from 'react';
 import './StarWars.scss';
 
-const StarWars = props => {
-    return (
-        <div className="character-card">
-            <h3>{props.character.name}</h3>
-            <p>
-                <strong>Birth Year:</strong> {props.character.birth_year}
-            </p>
-            <p>
-                <strong>Gender:</strong> {props.character.gender}
-            </p>
-            <p>
-                <strong>Height:</strong> {props.character.height}cm
-            </p>
-        </div>
-    );
+class StarWars extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            character: props.character,
+            homeworld: "",
+            species: "",
+        };
+    }
+
+    getHomeworld = URL => {
+        fetch(URL)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({ homeworld: data.name });
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    };
+
+    getSpecies = URL => {
+        fetch(URL)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({ species: data.name });
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    };
+
+    componentDidMount() {
+        this.getHomeworld(this.state.character.homeworld);
+        this.getSpecies(this.state.character.species);
+    }
+
+    render() {
+        return (
+            <div className="character-card">
+                <h3>{this.state.character.name}</h3>
+                <p>
+                    <strong>Birth Year:</strong> {this.state.character.birth_year}
+                </p>
+                <p>
+                    <strong>Gender:</strong> {this.state.character.gender}
+                </p>
+                <p>
+                    <strong>Height:</strong> {this.state.character.height}cm
+                </p>
+                <p>
+                    <strong>Homeworld:</strong> {this.state.homeworld}
+                </p>
+                <p>
+                    <strong>Species:</strong> {this.state.species}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default StarWars;
